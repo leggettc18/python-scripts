fddab4b85139b28c2482b8c074eb2bccaded2fd7
Christopher Leggett <leggettc18@gmail.com>
2018-05-21 12:38:22 -0400

    Initial commit for madlibs program

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# madlibs.py - Takes a text file and replaces the words ADJECTIVE, NOUN, 
+# ADVERB, and VERB with user input. Like Mad Libs.
+
+# TODO: Open text file
+
+# TODO: Scan the file for occurrences of ADJECTIVE, NOUN, ADVERB, or VERB while
+# copying content to a new file.
+
+# TODO: When a keyword is found, prompt the user for a replacement word
+
+# TODO: Put the input into a new file
